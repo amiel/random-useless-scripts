d69d2e220631a52acdb735d1c3e2ab33947d5e57
Amiel Martin <amiel@wlan121-115.wlan.wwu.edu>
2009-03-01 18:50:52 -0800

    pl.c, perl and c in the same file (note that there ore no semi-colons in this file)

---
+++ pl.c
@@ -0,0 +1,23 @@
+#define split =='/'-'%'?_=('+'+'+'+'('):'>'>'>')||(_
+#define while main(_){while(((_=getchar())&&((_
+while
+#define while \
+(@ a=
+split
+#ifndef __STRICT_ANSI__
+//,<STDIN>){while(@a&&print(chr(($a=ord(shift@a))==10?126:$a
+#endif
+==126?
+#define a /*
+10
+:(
+$a>126||$
+a<32
+)?$
+a:125
+-($
+#*/_='`'-'/'-'\'':'<'<'<'))||(_=('-'-'-'-32+_<'%'%'%'||_>'+'+'+'+')')?_:'}'-(_-32)))&&_!='('-')'&&putchar(_
+a
+#define split \
+-32))
+)){}}
